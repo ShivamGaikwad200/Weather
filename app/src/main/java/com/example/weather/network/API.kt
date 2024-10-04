b175124b0132d321f9a9608df24334618a5803b9
ShivamGaikwad200 <shivamgaikwad200@gmail.com>
2024-10-04 15:31:00 +0530

    finished project

--- app/src/main/java/com/example/weather/network/API.kt
+++ app/src/main/java/com/example/weather/network/API.kt
@@ -1,4 +1,17 @@
 package com.example.weather.network
 
+import com.example.weather.models.Location
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val APIKEY = "8lQbZLgO7xPiEQzNSubK4sjLRFLDIPEC\n"
+
 interface API {
+
+        @GET("locations/v1/cities/search")
+    suspend fun searchLocation(
+        @Query("apikey") apiKey: String = APIKEY,
+        @Query("q") query: String
+    ): Response<List<Location>>
 }
